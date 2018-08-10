462c0d025e50c1a05fa08d968836c8ff2a41e2b0
Marcin Latos <marcin.latos@espeo.eu>
2018-08-10 06:31:47 +0200

    Pilot Episode.

---
+++ README.md
@@ -0,0 +1,8 @@
+# flutter_evelopment_show
+
+Flutter Development Show
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/src/article.dart
@@ -0,0 +1,45 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentsCount;
+
+  const Article(this.text, this.domain, this.by, this.age, this.score,
+      this.commentsCount);
+}
+
+final articles = [
+  new Article("Wiadomosci z Onet", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Wypadek autobusu w trakcie wycieczki do Koziej Wólki", "wp.pl", "Lee Chan", "3 hours", 32, 41),
+  new Article("Nowe kosmetyki dla kazdego", "o2.pl", "Joanna Dinks", "2 hours", 54, 12),
+  new Article("Power Worth Less Than Zero Spreads as Green Energy Floods the Grid", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Snap’s Drop in Active Users Could Signal a Social Media Peak", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("To Remember Everything You Learn, Surrender to This Algorithm (2008) ", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Facebook’s Teen App Used a Psychological Trick to Attract High School Downloads", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Wiadomosci z Onet", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Wiadomosci z Onet", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Wiadomosci z Onet", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Wiadomosci z Onet", "onet.pl", "John Go", "7 hours", 1232, 45),
+  new Article("Wiadomosci z Onet", "onet.pl", "John Go", "7 hours", 1232, 45),
+];
